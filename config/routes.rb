7897a794e0ba59f8950867e350e1b180a95c4407
Wellington <wdcunha@gmail.com>
2017-12-15 19:49:20 -0800

    Final edition

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get('/', { to: 'posts#index', as: :home})
-  # get('/posts', { to: 'posts#index'})
+  # resource :posts #, only: [:new, :create, :destroy]
 
+
+  get('/', { to: 'posts#index', as: :home})
+  get('/posts/new', { to: 'posts#new', as: :new_post})
+  post('/posts', { to: 'posts#create', as: :posts })
+  get('/posts', { to: 'posts#index'})
   get('/posts/:id', { to: 'posts#show', as: :post})
+  delete('/posts/:id', to: 'posts#destroy')
+  get('/posts/:id/edit', to: 'posts#edit', as: :edit_post)
+  patch('/posts/:id', to: 'posts#update')
+
+
+  # get('/', to: 'posts#index')
+  # get('/posts/new', to: 'posts#new', as: :new_post)
+  # post('/posts/', to: 'posts#create', as: :posts)
+  # get('/posts/', to: 'posts#index')
+  # get('/posts/:id', to: 'posts#show', as: :post)
 
 end
