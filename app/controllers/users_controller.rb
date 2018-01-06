42789ac29eb199973947bfc46ba27ab3190e76b6
Wellington <wdcunha@gmail.com>
2018-01-05 19:38:08 -0800

    finished change password and all the homework

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :find_user, only: [:show, :edit, :update, :destroy]
+before_action :find_user, only: [:show, :edit, :update, :destroy, :update_password]
 
   def new
     @user = User.new
@@ -31,17 +31,41 @@ before_action :find_user, only: [:show, :edit, :update, :destroy]
   end
 
   def edit_password
-
+    @user = current_user
   end
+
+  # def update_password
+  #   @user = current_user
+  #   if params[:current_password].empty?
+  #     @user.errors.add(:current_password, "can't be empty")
+  #     render 'edit_password'
+  #   elsif @user.authenticate(password_params[:password] &&
+  #         password_params[:new_password] != password_params[:current_password])
+  #     @user.password = password_params[:new_password]
+  #     if @user.save
+  #       flash[:success] = "Password was changed successfully"
+  #       redirect_to home_path
+  #     end
+  #   else
+  #     flash[:danger] = "Failed"
+  #     render 'edit_password'
+  #   end
+  # end
 
   def update_password
-    if params[:user][:password].empty?
-      @user.errors.add(:current_password, "can't be empty")
-      render 'edit_password'
-    elseif @
+      @user = current_user
 
-    end
-  end
+      if @user.authenticate(password_params[:current_password]) &&
+        password_params[:new_password] != password_params[:current_password]
+        @user.password = password_params[:new_password]
+        @user.save!
+        flash[:notice] = "Password was changed successfully"
+        redirect_to home_path
+       else
+         flash[:alert] = "Wrong password"
+         render :edit_password
+       end
+     end
 
   private
   def user_params
@@ -52,6 +76,11 @@ before_action :find_user, only: [:show, :edit, :update, :destroy]
       :password,
       :password_confirmation
     )
+  end
+
+  #  change password
+  def password_params
+      params.permit(:current_password, :new_password, :new_password_confirmation)
   end
 
   def find_user
